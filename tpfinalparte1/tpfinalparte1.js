f436bf381632f172e10085c210d201448af955f3
matildaguida <guidamatilda0@gmail.com>
2025-09-26 20:23:29 -0300

    Add files via upload

---
+++ tpfinalparte1/tpfinalparte1.js
@@ -0,0 +1,69 @@
+
+let portada;
+
+
+function preload() {
+  
+  portada = loadImage(".assets/portada.jpg");
+  
+}
+
+
+function setup() {
+   createCanvas(640,480);
+   
+}
+
+
+function draw() {
+  image(portada, 0, 0);
+  
+  
+  
+   if( estado == 0 ){
+    // imagen de fondo
+    // evento: botones
+    // otras cosas a visualizar en pantalla
+  }else if( estado == 1 ){
+   // imagen de fondo
+    // evento: botones
+    // otras cosas a visualizar en pantalla
+  }else if( estado == 2 ){
+   // imagen de fondo
+    // evento: botones
+    // otras cosas a visualizar en pantalla
+  }else if( estado == 3 ){
+        // imagen de fondo
+    // evento: botones
+    // otras cosas a visualizar en pantalla
+
+  }else if( estado == 4 ){
+// imagen de fondo
+    // evento: botones
+    // otras cosas a visualizar en pantalla
+  }else if( estado == 5 ){
+   // imagen de fondo
+    // evento: botones
+    // otras cosas a visualizar en pantalla
+  }else if( estado == 6 ){
+    // imagen de fondo
+    // evento: botones
+    // otras cosas a visualizar en pantalla
+  }else if( estado == 7 ){
+   // imagen de fondo
+    // evento: botones
+    // otras cosas a visualizar en pantalla/
+  }else if( estado == 8 ){
+   // imagen de fondo
+    // evento: botones
+    // otras cosas a visualizar en pantalla
+  }else if( estado == 9 ){
+    // imagen de fondo
+    // evento: botones
+    // otras cosas a visualizar en pantalla
+  }else if( estado == 10 ){
+   // imagen de fondo
+    // evento: botones
+    // otras cosas a visualizar en pantalla
+  }
+}
