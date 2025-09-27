6e4ef525c0fbe1cbd198f5b3a810ebe7053861eb
luz moral <142944434+luzmoral@users.noreply.github.com>
2025-09-27 18:52:37 -0300

    modificacion nueva
    cambio de imagenes con movimiento por tiempo

--- tpfinalparte1/tpfinalparte1.js
+++ tpfinalparte1/tpfinalparte1.js
@@ -1,23 +1,25 @@
 let portada;
+let imagenes = [];
+let numero = [
+"1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg","7.jpg","8.jpeg","9.jpeg","10.jpg","11.jpg","12.jpg","13.jpeg","14.jpeg","15.jpg","16.jpg","17.jpg","18.jpg","19.jpg","20.jpg","21.jpeg"];
 let estado = 0;// estado inicial
 
 function preload() {
-  
   portada = loadImage("assets/portada.jpg");
   
- 
+  for (let i = 0; i < numero.length; i++) {
+    imagenes[i] = loadImage("assets/"+ numero[i]);
 }
-
+}
 
 function setup() {
    createCanvas(640,480);
-   
+   frameRate(60);//aseguramos 60 FPS 
 }
 
-
 function draw() {
+  
   background(255);
-
   
    if( estado == 0 ){
      image(portada,0,0,width, height);// imagen de fondo
@@ -33,13 +35,21 @@ function draw() {
      text("pulse espacio para comenzar ",width/2,height -80); // evento: botones
      text("creditos  ",width/2,height -25); // evento: botones
    
-     
-  }else if( estado == 1){
-    // imagen de fondo// imagen de fondo
+  }else if( estado >= 1 && estado <=numero.length){
+    image(imagenes[estado - 1],0,0, width,height);// imagen de fondo// imagen de fondo
     // evento: botones
     // otras cosas a visualizar en pantalla
-    
-  }else if( estado == 2 ){
+  }
+  if (frameCount %(60 * 3 ) == 0) { //cada 180 frames
+    estado++;
+    if (estado > numero.length){
+      estado = 0; //vuelve a portada 
+      
+    }
+  }
+}
+  
+ /* else if( estado == 2 ){
    // imagen de fondo
     // evento: botones
     // otras cosas a visualizar en pantalla
@@ -118,14 +128,16 @@ function draw() {
    // imagen de fondo
     // evento: botones
     // otras cosas a visualizar en pantalla
+  }else if( estado == 21 ){
+   // imagen de fondo
+    // evento: botones
+    // otras cosas a visualizar en pantalla
   }
+  
  function keyPressed(){
    if (key === ''){ // espacio para avanzar 
    estado++;
    if (estado >20){
      estado = 0; //vuelve al inicio
    }
-  }
- }
-}
-
+  }*/
