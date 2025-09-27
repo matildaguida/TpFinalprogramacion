d46f0a5197c775b16b07e90ebf4705a1a2f0e304
luz moral <142944434+luzmoral@users.noreply.github.com>
2025-09-27 15:06:10 -0300

    Update tpfinalparte1.js

--- tpfinalparte1/tpfinalparte1.js
+++ tpfinalparte1/tpfinalparte1.js
@@ -1,14 +1,8 @@
-//hola
-
-// luzzz tp
-
-//hola 2
 let portada;
 let estado = 0;// estado inicial
 
 function preload() {
   
-  portada = loadImage(".assets/portada.jpg");
   portada = loadImage("assets/portada.jpg");
   
  
@@ -23,7 +17,6 @@ function setup() {
 
 function draw() {
   background(255);
-  image(portada, 0, 0);
 
   
    if( estado == 0 ){
